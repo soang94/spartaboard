5ce5d0bc213ab96d2b7d29ddbbb1377e21955563
이승상 <soang2533@gmail.com>
2024-02-01 15:35:22 +0900

    first commit

---
+++ src/main/kotlin/org/example/spartaboard/SpartaboardApplication.kt
@@ -0,0 +1,11 @@
+package org.example.spartaboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpartaboardApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpartaboardApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/spartaboard/SpartaboardApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.spartaboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpartaboardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
